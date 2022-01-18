edf154e034cc572550ecc7fa8869a94af9cc7e97
IvanDanylchenko <danylchenko21@gmail.com>
2022-01-18 22:40:44 +0200

    Add task objects 1-3

---
+++ js/index.js
@@ -0,0 +1,55 @@
+console.group("Task 1");
+
+const student = {
+  firstName: "Terry",
+  lastName: "Clinton",
+  isMale: "Boy",
+  email: "john0@gmail.com",
+  address: "90 Bedford St, Manhattan",
+};
+
+console.log(student.address);
+student.isMale = "Girl";
+
+console.groupEnd();
+
+console.group("Task 2");
+
+const department = {
+  titleFaculty: "Факультет прикладних наук",
+  facultyPartners: "Epam, Sigma, SoftSereve",
+  link: "https://apps.ucu.edu.ua/",
+  trainingProgram: "Компьютерные науки",
+  email: "mscs@ucu.edu.ua",
+};
+
+const getFaculty = "titleFaculty";
+department[getFaculty] = "Faculty of Applied Sciences";
+
+console.groupEnd();
+
+console.group("Task 3");
+
+function Books(author, title, yearOfPublishing, publishingHouse) {
+  this.author = author;
+  this.title = title;
+  this.yearOfPublishing = yearOfPublishing;
+  this.publishingHouse = publishingHouse;
+}
+
+const booksProto = {};
+
+booksProto.yearsOld = function () {
+  return (this.yearOfPublishing = 2022 - this.yearOfPublishing);
+};
+
+Books.prototype = booksProto;
+
+const book1 = new Books(
+  "Charles Dickens",
+  "A Christmas Carol. In Prose. Being a Ghost Story of Christmas",
+  1843,
+  "Chapman & Hall"
+);
+
+console.groupEnd();
